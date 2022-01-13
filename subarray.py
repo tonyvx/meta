e3a9662bf0352595b114615f11c06aaed56c61db
tonyvx <tonyvx@avx007.org>
2022-01-13 09:16:06 -0500

    Completed

--- subarray.py
+++ subarray.py
@@ -1,5 +1,29 @@
-def say_hello():
-  print('Hello, Worls')
+def sub_array(arr, k):
+    sub = []
 
-for i in range(5):
-  say_hello()
+    for idx, val in enumerate(arr):
+        # initialize
+        temp = int(val)
+        i = idx
+        new_arr = [temp]
+
+        # loop backwards if current value not equal to k and exit once its equal or greater
+        while ((temp < k) & (i > 0)):
+            i = i-1
+            temp = temp + int(arr[i])
+            new_arr.append(int(arr[i]))
+
+        # if sub arr sum is equal to k add to valid sub arrays
+        if (temp == k):
+            sub.append(new_arr)
+
+    # publish sub arrays for k
+    print("\n\nOutput:\nFor k = ", k, " found ", len(sub),
+          " sub arrays \n\n Sub Array Details: \n", sub)
+
+
+print('Enter array separated by space: ')
+arr = input().split(" ")
+print('Enter k: ')
+k = input()
+sub_array(arr, int(k))
